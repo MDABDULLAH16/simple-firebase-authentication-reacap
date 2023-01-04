ff24373e2d9064dce7649116b7d3af80bc16492d
MDABDULLAH16 <mdabdullah161036@gmail.com>
2023-01-04 20:07:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { getAuth } from "firebase/auth";
+import app from "./firebase.init";
+
+const auth = getAuth(app);
 
 function App() {
+  const handleSignIn = () => {
+    console.log("working with google auth");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is app js with authentication</h1>
+      <button onClick={handleSignIn}>Sign in with Google</button>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqQiZg2C9ozE9ky0krS-UQ3WFu23n3gx4",
+  authDomain: "simple-firebase-auth-rec-aa433.firebaseapp.com",
+  projectId: "simple-firebase-auth-rec-aa433",
+  storageBucket: "simple-firebase-auth-rec-aa433.appspot.com",
+  messagingSenderId: "675177351960",
+  appId: "1:675177351960:web:bb732d83173994a0516c6d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
