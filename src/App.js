c95b77f6f76d65d9c5e7cd174f14117b9f6742aa
MDABDULLAH16 <mdabdullah161036@gmail.com>
2023-01-04 21:24:14 +0600

    successfully sign out with simple code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import "./App.css";
 import app from "./firebase.init";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+
   const provider = new GoogleAuthProvider();
   const handleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        setUser(result.user);
+        console.log(result.user);
       })
       .catch((error) => {
         console.log(error);
       });
   };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        setUser({});
+      })
+      .catch((error) => {
+        setUser({});
+      });
+  };
+
   return (
     <div className="App">
       <h1>This is app js with authentication</h1>
       <button onClick={handleSignIn}>Sign in with Google</button>
+      <button onClick={handleSignOut}>Sign Out</button>
+      <h2>Name: {user.displayName}</h2>
+      <p>Email: {user.email}</p>
+      <img referrerPolicy="no-referrer" src={user.photoURL} alt="" />
     </div>
   );
 }
